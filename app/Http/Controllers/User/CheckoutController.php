497022e84a3e7fac4c860d6c9e5e96292498b785
= <=>
2024-01-28 16:15:52 +0700

    Integrate Checkout

---
+++ app/Http/Controllers/User/CheckoutController.php
@@ -0,0 +1,96 @@
+<?php
+
+namespace App\Http\Controllers\User;
+
+use App\Http\Controllers\Controller;
+use App\Models\Camp;
+use App\Models\Checkout;
+use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Auth;
+
+class CheckoutController extends Controller
+{
+    /**
+     * Display a listing of the resource.
+     */
+    public function index()
+    {
+        //
+    }
+
+    /**
+     * Show the form for creating a new resource.
+     */
+    public function create(Camp $camp)
+    {
+        return view('checkout/create', [
+            'camp' => $camp
+        ]);
+    }
+
+    /**
+     * Store a newly created resource in storage.
+     */
+    public function store(Request $request, Camp $camp)
+    {
+        // Mapping request data
+        $data = $request->all();
+        $data['user_id'] = Auth::id();
+        $data['camp_id'] = $camp->id;
+        $data['name'] = $data['name'];
+        $data['email'] = $data['email'];
+        $data['occupation'] = $data['occupation'];
+        $data['card_number'] = $data['card_number'];
+        $data['expired'] = $data['expired'];
+        $data['cvc'] = $data['cvc'];
+
+        // Update User Data
+        $user = Auth::user();
+        $user->email = $data['email'];
+        $user->name = $data['name'];
+        $user->occupation = $data['occupation'];
+        $user->save();
+
+        // Create Checkout
+        $checkout = Checkout::create($data);
+
+        return redirect()->route('checkout.success');
+    }
+
+    /**
+     * Display the specified resource.
+     */
+    public function show(Checkout $checkout)
+    {
+        //
+    }
+
+    /**
+     * Show the form for editing the specified resource.
+     */
+    public function edit(Checkout $checkout)
+    {
+        //
+    }
+
+    /**
+     * Update the specified resource in storage.
+     */
+    public function update(Request $request, Checkout $checkout)
+    {
+        //
+    }
+
+    /**
+     * Remove the specified resource from storage.
+     */
+    public function destroy(Checkout $checkout)
+    {
+        //
+    }
+
+    public function success()
+    {
+        return view('checkout/success');
+    }
+}
--- routes/web.php
+++ routes/web.php
@@ -1,6 +1,7 @@
 <?php
 
 use App\Http\Controllers\ProfileController;
+use App\Http\Controllers\User\CheckoutController;
 use App\Http\Controllers\UserController;
 use Illuminate\Support\Facades\Route;
 
@@ -19,32 +20,16 @@ Route::get('/', function () {
     return view('index');
 })->name('home');
 
-Route::get('/login', function () {
-    return view('login');
-})->name('login');
-
-Route::get('/checkout', function () {
-    return view('checkout');
-})->name('checkout');
-
-
-Route::get('/success-checkout', function () {
-    return view('success_checkout');
-})->name('success-checkout');
-
-Route::get('/dashboard', function () {
-    return view('dashboard');
-})->middleware(['auth', 'verified'])->name('dashboard');
-
+Route::get('/checkout/success', [CheckoutController::class, 'success'])->name('checkout.success');
+Route::get('/checkout/{camp:slug}', [CheckoutController::class, 'create'])->name('checkout.create');
+Route::post('/checkout/{camp}', [CheckoutController::class, 'store'])->name('checkout.create');
 
 // Socialite Routes
 Route::get('/login/google', [UserController::class, 'google'])->name('login.google');
 Route::get('/auth/google/callback', [UserController::class, 'googleCallback'])->name('login.google.callback');
 
-Route::middleware('auth')->group(function () {
-    Route::get('/profile', [ProfileController::class, 'edit'])->name('profile.edit');
-    Route::patch('/profile', [ProfileController::class, 'update'])->name('profile.update');
-    Route::delete('/profile', [ProfileController::class, 'destroy'])->name('profile.destroy');
-});
+Route::get('/dashboard', function () {
+    return view('dashboard');
+})->middleware(['auth', 'verified'])->name('dashboard');
 
 require __DIR__.'/auth.php';
